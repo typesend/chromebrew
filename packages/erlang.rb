7d9c43ecdbe75e0712e67fbeeadae3e90e28d1ee
Ben Damman <ben.damman@gmail.com>
2017-05-21 01:55:19 -0700

    Update erlang.rb

--- packages/erlang.rb
+++ packages/erlang.rb
@@ -4,6 +4,8 @@ class Erlang < Package
   version '19.2'
   source_url 'http://www.erlang.org/download/otp_src_19.2.tar.gz'
   source_sha1 'f5188ba6f496b9d1c37597705d095b4e6aa7bcd3'
+  
+  depends_on 'perl'
 
   def self.build
     system 'export ERL_OTP=`pwd`'
