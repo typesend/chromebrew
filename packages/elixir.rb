1ae0745e349d4ea50321952bcb69baf5062117ee
Ben Damman <ben.damman@gmail.com>
2017-05-21 05:10:25 -0400

    Update elixir.rb

--- packages/elixir.rb
+++ packages/elixir.rb
@@ -5,7 +5,7 @@ class Elixir < Package
   depends_on 'erlang'
 
   source_url 'https://github.com/elixir-lang/elixir/archive/v1.4.4.tar.gz'
-  source_sha1 '867da6f876c48279823a41a33030c95ef37379b2'
+  source_sha1 'cc2f673b374bbd51ebd4f8cfaaa83728cc63829f'
 
   def self.build
     system 'make clean test'
